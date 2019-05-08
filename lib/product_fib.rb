99ef40be0329ba5603a96e527697d621c4f87cf3
Shendidy <sherif@shendidy.com>
2019-05-08 04:34:48 +0100

    clever and beautiful answer found on codewars

--- lib/product_fib.rb
+++ lib/product_fib.rb
@@ -1,32 +1,10 @@
 class Product_Fib
   def productFib(prod)
-    pro_fib(prod)[0] * pro_fib(prod)[1] == prod ? 
-      [pro_fib(prod)[0], pro_fib(prod)[1], true] :
-      [pro_fib(prod)[0], pro_fib(prod)[1], false]
+    a, b = [0, 1]
+    while prod > a * b
+      a, b = [b, a + b]
+    end
+    [a, b, prod == a * b]
   end
 
-  private
-
-  def pro_fib(prod)
-    ind = 0
-    arr =[]
-    loop {
-      arr = fib(ind)
-      break if arr[0] * arr[1] >= prod
-      ind += 1
-    }
-    [arr[0], arr[1]]
-  end
-
-  def fib(ind)
-    nums = []
-    min = helper = 0
-    max = 1
-    ind.times {
-      helper = max
-      max += min
-      min = helper
-    }
-    return [min, max]
-  end
 end
