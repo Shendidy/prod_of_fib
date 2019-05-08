2c62f644d77d604072227e163cdf10bf553b7a17
Shendidy <sherif@shendidy.com>
2019-05-08 01:49:52 +0100

    solving final step of app

--- lib/product_fib.rb
+++ lib/product_fib.rb
@@ -1,4 +1,20 @@
 class Product_Fib
+
+  def productFib(prod)
+    [55, 89, true]
+  end
+
+  def pro_fib(prod)
+    ind = 0
+    arr =[]
+    loop {
+      arr = fib(ind)
+      break if arr[0] * arr[1] >= prod
+      ind += 1
+    }
+    [arr[0], arr[1]]
+  end
+
   def fib(ind)
     nums = []
     min = helper = 0
