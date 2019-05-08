073ad8ad6d413b14fa3a36b4f7e6d5e881c0aa93
Shendidy <sherif@shendidy.com>
2019-05-08 01:30:34 +0100

    fib return fib numbers based on given index

--- lib/product_fib.rb
+++ lib/product_fib.rb
@@ -1,5 +1,13 @@
 class Product_Fib
   def fib(ind)
-    [21, 34]
+    nums = []
+    min = helper = 0
+    max = 1
+    ind.times {
+      helper = max
+      max += min
+      min = helper
+    }
+    return [min, max]
   end
 end
--- spec/product_fib_spec.rb
+++ spec/product_fib_spec.rb
@@ -4,4 +4,8 @@ describe Product_Fib do
   it '1- should return [21, 34]' do
     expect(subject.fib(8)).to eq([21, 34])
   end
+
+  it '2- should return [1, 2]' do
+    expect(subject.fib(2)).to eq([1, 2])
+  end
 end
